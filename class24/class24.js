9e9e84a16292641eaa614377241e2a4e467e06c3
Sidhartha_sahani <sidharthasahanii@gmail.com>
2025-04-30 17:06:37 +0545

    update js

--- class24/class24.js
+++ class24/class24.js
@@ -28,34 +28,37 @@
 let input = document.querySelector("input");
 let btn = document.querySelector("button");
 let ul = document.querySelector("ul");
-
+ 
 btn.addEventListener("click", function () {
-
-    
 
     let item = document.createElement("li");
     item.innerText = input.value;
 
+
     let delBtn = document.createElement("button");
     delBtn.innerText = "delete";
+
     delBtn.classList.add("delete");
 
-    item.appendChild(delBtn);
-
+    item.append(delBtn);
     ul.appendChild(item);
 
-    input.value = ""; // this will reset input fild
-
+    console.log(input.value);
+    input.value = "";
 });
 
-
-let delBtns = document.querySelectorAll("button");
-for (delBtns of delBtns) {
-    delBtns.addEventListener("click", function () {
+let delBtns = document.querySelectorAll(".delete");
+for (delBtn of delBtns) {
+    delBtn.addEventListener("click", function () {
         let par = this.parentElement;
-        par.remove();
-    })
+        console.log(par);
+        par.remove()
+    });
 }
+
+
+
+
 
 
 
